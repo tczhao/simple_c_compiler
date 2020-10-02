219cf4e4ae8c0d554950c91308ab6fadacf07efd
tzhao <>
2020-10-02 10:19:49 +1000

    add maths/standard function and error handle

--- compiler.c
+++ compiler.c
@@ -67,6 +67,34 @@ int eval() {
         else if (op == ADJ)  {sp = sp + *pc++;}                               // to remove argument from stack frame
         else if (op == LEV)  {sp = bp; bp = (int *)*sp++; pc = (int *)*sp++;} // restore call frame and PC
         else if (op == LEA)  {ax = (int)(bp + *pc++);}                        // load address for arguments.
+        else if (op == OR)  ax = *sp++ | ax;                                  // Standard Maths
+        else if (op == XOR) ax = *sp++ ^ ax;
+        else if (op == AND) ax = *sp++ & ax;
+        else if (op == EQ)  ax = *sp++ == ax;
+        else if (op == NE)  ax = *sp++ != ax;
+        else if (op == LT)  ax = *sp++ < ax;
+        else if (op == LE)  ax = *sp++ <= ax;
+        else if (op == GT)  ax = *sp++ >  ax;
+        else if (op == GE)  ax = *sp++ >= ax;
+        else if (op == SHL) ax = *sp++ << ax;
+        else if (op == SHR) ax = *sp++ >> ax;
+        else if (op == ADD) ax = *sp++ + ax;
+        else if (op == SUB) ax = *sp++ - ax;
+        else if (op == MUL) ax = *sp++ * ax;
+        else if (op == DIV) ax = *sp++ / ax;
+        else if (op == MOD) ax = *sp++ % ax;
+        else if (op == EXIT) { printf("exit(%d)", *sp); return *sp;}          // Built in instructions
+        else if (op == OPEN) { ax = open((char *)sp[1], sp[0]); }
+        else if (op == CLOS) { ax = close(*sp);}
+        else if (op == READ) { ax = read(sp[2], (char *)sp[1], *sp); }
+        else if (op == PRTF) { tmp = sp + pc[1]; ax = printf((char *)tmp[-1], tmp[-2], tmp[-3], tmp[-4], tmp[-5], tmp[-6]); }
+        else if (op == MALC) { ax = (int)malloc(*sp);}
+        else if (op == MSET) { ax = (int)memset((char *)sp[2], sp[1], *sp);}
+        else if (op == MCMP) { ax = memcmp((char *)sp[2], (char *)sp[1], *sp);}
+        else {                                                                // Error handle
+            printf("unknown instruction:%d\n", op);
+            return -1;
+        }
     }
     return 0;
 }
